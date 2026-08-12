470af373d9bf17ed0127799087cb648a3bf6f187
Rohandalal8 <rohandalal079@gmail.com>
2026-08-12 23:43:23 +0530

    contact section added

--- portfolio/src/sections/Contact.tsx
+++ portfolio/src/sections/Contact.tsx
@@ -160,21 +160,21 @@ const Contact = () => {
                             </div>
                             <div className="button-holder">
                                 <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="currentColor"
-  strokeWidth="2"
-  strokeLinecap="round"
-  strokeLinejoin="round"
-  className="lucide lucide-linkedin w-7 h-7"
->
-  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4V9h4v5" />
-  <rect width="4" height="12" x="4" y="9" />
-  <circle cx="6" cy="4" r="2" />
-</svg>
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="lucide lucide-linkedin w-7 h-7"
+                                >
+                                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4V9h4v5" />
+                                    <rect width="4" height="12" x="4" y="9" />
+                                    <circle cx="6" cy="4" r="2" />
+                                </svg>
                                 <p>LinkedIn</p>
                             </div>
                         </button>
